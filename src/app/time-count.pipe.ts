6a0e41fe24c506a8e5d8902db14d128c3c5a06bc
dancan-sandys <dancan.oruko96@gmail.com>
2020-08-30 20:13:15 +0300

    add a pipe to calculate the number of hour a user has been using gihub

--- src/app/time-count.pipe.ts
+++ src/app/time-count.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeCountPipe implements PipeTransform {
 
-  transform(value:any): number {
-    return null;
+  transform(value: any): number {
+
+    let today: Date = new Date();
+    let todayWithNotime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    let difference: number = todayWithNotime - value
+    let differenceInSeconds = difference / 1000
+    let DifferenceInHours = differenceInSeconds / 3600
+
+    return DifferenceInHours;
   }
 
 }
