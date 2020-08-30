d503b45827cf450f51d7f4f37e81b0079a6f599a
dancan-sandys <dancan.oruko96@gmail.com>
2020-08-30 13:33:58 +0300

    add a routing module system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent} from './profile/profile.component'
+import { ReposComponent } from './repos/repos.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path:'profile', component:ProfileComponent},
+{path:'repos', component:ReposComponent},
+{path:'', redirectTo:'/profile', pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/repos/repos.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-repos',
+  templateUrl: './repos.component.html',
+  styleUrls: ['./repos.component.css']
+})
+export class ReposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
